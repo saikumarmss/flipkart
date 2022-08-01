43589308461b2f839558d402194dd73b93209a0f
saikumarmss <saikumarlrg@gmail.com>
2022-08-01 12:30:19 +0530

    new commit

--- dev.java
+++ dev.java
@@ -3,5 +3,9 @@
  this is updated in deveploper branch
  hi this is 3rd time updating the file in master branch
  this is updated in deveploper branch 2nd time
+<<<<<<< Updated upstream
  this is 3rd time updating dev file in developer branch
  it was updated in developer branch 2n time
+=======
+ hj this is 3rd time updating dev file in developer branch
+>>>>>>> Stashed changes
